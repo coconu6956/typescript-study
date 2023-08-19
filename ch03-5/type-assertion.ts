329578eac678cecde5ad243250b8127cc049d043
coconu6956 <wce0422@gmail.com>
2023-08-19 17:19:47 +0900

    파일 정리

--- ch03-5/type-assertion.ts
+++
@@ -1,8 +0,0 @@
-import INameable from "./INameable";
-
-let obj: object = {name: 'Jack'}
-
-let name1 = (<INameable>obj).name
-let name2 = (obj as INameable).name
-
-console.log(name1, name2)
--- ch03/Person2.ts
+++
@@ -1,6 +0,0 @@
-class Person2 {
-  constructor(public name: string, public age?: number) {}
-}
-
-let jack2: Person2 = new Person2('jack', 32)
-console.log(jack2)
